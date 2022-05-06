fd9574b9a04044fd332f4a65d36d5d52eb84520e
gonrod1 <gonzalo.rodriguez@dualbootpartners.com>
2022-05-06 10:40:34 -0300

    Create and destroy session for users

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :users, exept: [:new]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  get 'logout', to: 'sessions#destroy'
 end
