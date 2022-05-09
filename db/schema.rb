172286fefd1f6fa1e6959a26005e2ddd43dfc2b8
gonrod1 <gonzalo.rodriguez@dualbootpartners.com>
2022-05-09 15:37:23 -0300

    bd2

--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_09_124827) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "user_id"
   end
 
